832b2e1770611ac6b931239186511cc93a063177
Ramadan Cilomba <rcilomba@yahoo.com>
2023-08-24 11:18:50 +0200

    components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Works from './components/Works';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+      <Works />
+      <Contact />
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/components/Works.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Works() {
+    return(
+        <div>
+            About
+        </div>
+    )
+}
+export default Works;
