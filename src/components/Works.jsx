262ee1399f6ebc114b5da2fb6e9434242b1a98a2
rcilomba <rcilomba@yahoo.com>
2024-10-21 10:50:38 +0200

    updates work links

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -194,7 +194,7 @@ function Works() {
                     Demo
                   </button>
                 </a>
-                <a href="#">
+                <a href="/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
