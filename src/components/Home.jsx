c1e01ab54899a0a0c7636d2cfb77db9658638987
rcilomba <rcilomba@yahoo.com>
2026-02-26 05:33:09 +0100

    size update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
     <div name='home' className='h-screen w-full bg-[#062419]'>
       <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
         <div className='flex flex-col justify-center h-full'>
-          <h2 className='text-4xl sm:text-7xl font-bold text-white'>
+          <h2 className='text-4xl sm:text-6xl font-bold text-white'>
             Improving digital systems and workflows
           </h2>
           <p className='text-gray-500 py-4 max-w-md'>
